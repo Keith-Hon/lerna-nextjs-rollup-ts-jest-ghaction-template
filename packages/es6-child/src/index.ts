e5bd6b350d1e82b7d3f7aea7828e93b5cca7df96
Keith Hon <keith.wkhon@gmail.com>
2022-04-03 16:29:57 +0800

    added agadoo for treeshake check

--- packages/es6-child/src/index.ts
+++ packages/es6-child/src/index.ts
@@ -1,4 +1,10 @@
-import { CustomType } from "es6-pkg-template";
+import { CustomType, addOne } from "es6-pkg-template";
+
+export function addTwo({ array }: {
+    array: CustomType[];
+}): CustomType[] {
+    return addOne({ array: addOne({ array }) });
+}
 
 export function multiplyTwo({ array }: { array: CustomType[] }): CustomType[] {
     return array.map((ele) => ({ name: ele.name, count: ele.count * 2 }));
